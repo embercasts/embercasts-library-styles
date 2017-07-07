5a239bb1ea64207ae939751533681a79c4671678
Ryan Tablada <ryan.tablada@gmail.com>
2017-07-07 16:20:37 -0500

    move images to public

--- index.js
+++ index.js
@@ -3,10 +3,4 @@
 
 module.exports = {
   name: 'embercasts-library-styles',
-
-  included(app) {
-    app.import('vendor/logo.svg');
-    app.import('vendor/favicon.png');
-    app.import('vendor/welcome.svg');
-  }
 };
