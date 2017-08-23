d43d82c4707f2bdea8d5d14128a144de9e7f9cf6
Ryan Tablada <ryan.tablada@gmail.com>
2017-08-23 17:51:01 -0500

    Update blueprint for <2.16

--- blueprints/embercasts-library-styles/index.js
+++ blueprints/embercasts-library-styles/index.js
@@ -1,4 +1,6 @@
 /* eslint-env node */
+const semver = require('semver');
+
 module.exports = {
   description: '',
 
@@ -6,11 +8,19 @@ module.exports = {
 
   },
 
-  afterInstall: function() {
-    return this.addPackagesToProject(
-      [
-        { name: 'ember-cli-sass' }
-      ]
-    );
+  afterInstall: function({ project }) {
+    const emberSource = project.addonPackages['ember-source'];
+    const emberVersion = emberSource.pkg.version;
+
+    const packages = [
+      { name: 'ember-cli-sass' },
+    ];
+
+    // Installs Ember Import Blueprints if < 2.16
+    if (semver.lt(emberVersion, '2.16.0')) {
+      packages.push({ name: 'ember-import-blueprints' })
+    }
+
+    return this.addPackagesToProject(packages);
   }
 };
